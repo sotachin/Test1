dfb8b5dea83af52246e230e80546ed645e1830bf
SotaChin <chin.sonita--3@gmail.com>
2023-12-02 10:51:23 +0700

    Second commit

---
+++ test2301/Controllers/DocumentController.cs
@@ -0,0 +1,29 @@
+﻿using System.Web.Mvc;
+
+namespace test2301.Controllers
+{
+    public class DocumentController : Controller
+    {
+        // GET: Document
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult show()
+        {
+            return File(Server.MapPath("~/Content/test.txt"), "text/plain");
+        }
+        public FileResult Download()
+        {
+            return File(Server.MapPath("~/Content/test.txt"), "text/plain", "myfile.text");
+        }
+        public RedirectResult ShowInternal()
+        {
+            return Redirect("/Home/ShowInfo");
+        }
+        public RedirectResult ShowExternal()
+        {
+            return Redirect("https://google.com");
+        }
+    }
+}

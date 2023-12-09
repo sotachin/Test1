caec88ebac2d52ac4abbfd9f424fd9fb0829d6c4
SotaChin <chin.sonita--3@gmail.com>
2023-12-09 15:14:49 +0700

    (test commit)

--- test2301/Controllers/ItemController.cs
+++ test2301/Controllers/ItemController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using test2301.Models;
 
 namespace test2301.Controllers
 {
@@ -11,7 +12,19 @@ namespace test2301.Controllers
         // GET: Item
         public ActionResult Index()
         {
-            return View();
+            ViewBag.shopName = "Computer Shop";
+            List<ItemList> itemlist = new List<ItemList>()
+            {
+                new ItemList { ID = "001", Name = "Acer", Price = 500, Category = "Laptop" },
+                new ItemList { ID = "002", Name = "Mac", Price = 1500, Category = "Laptop" },
+                new ItemList { ID = "003", Name = "Asus", Price = 600, Category = "Laptop" },
+                new ItemList { ID = "004", Name = "Dell", Price = 500, Category = "Laptop" },
+
+
+            };
+            itemlist.Add(new ItemList { ID = "005", Name = "MSI", Price = 900, Category = "Laptop" });
+
+            return View(itemlist);
         }
     }
 }
--- test2301/Models/ItemList.cs
+++ test2301/Models/ItemList.cs
@@ -11,8 +11,7 @@ namespace test2301.Models
         public string Name { get; set; }
         public float Price { get; set; }
         public string Category { get; set; }
-    {
+    
 
     }
     }
-}
